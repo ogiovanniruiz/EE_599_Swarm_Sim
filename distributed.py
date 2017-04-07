b3219e7fb2222041c8d4e5fe87681f6e92d7b035
ogiovanniruiz <ogiovanniruiz@gmail.com>
2017-04-06 17:14:28 -0700

    Introduce Spacers capped multiplier

--- distributed.py
+++ distributed.py
@@ -17,43 +17,49 @@ black = [0, 0, 0]
 
 SCREENSIZE = [1200, 600]  # Size of our output display
 
-N = 12#Number of Robots
+N = 10 #Number of Robots
 Dim = 2  #Dimension of search space
 bot_radius = 5
 
 running = True
 
-W = np.zeros((N, N), dtype=np.float) # The Swarm Position Weights Matrix
-V = np.zeros((N, N), dtype=np.float) # The Swarm Position Weights Matrix
-A = np.zeros((N, N), dtype=np.float) # The Swarm Position Weights Matrix
+W = np.zeros((N, N), dtype=np.float) # The Swarm Weights Matrix
+V = np.zeros((N, N), dtype=np.float) # The Swarm Energy Matrix
+A = np.zeros((N, N), dtype=np.float) # The Swarm Adjacency Matrix
 K = 500
 
+# The following Configures the Adjacency Matrix:
 for i in range(N):
     for j in range(N):
 
         if (i == j):
 
-            A[i, j] = 0
+            #Diagonal Elements are not important for adjacency because they represent the robot's connections to itself.
+            A[i, j] = 0 
 
         elif (i == j + 1) or (i == j - 1):
 
-            A[i,j] = 1
+            #Robots should be connected to their immediate neighbors.
+            A[i,j] = 1 
 
 
         elif ((i == N - 1) and (j == 0)) or ((i == 0) and (j == N - 1)):
 
-            A[i,j] = 1
+            #Robot 1 and N are immediate neighbors to each other.
+            A[i,j] = 1 
 
         elif (i == j + 2) or (i == j - 2) or ((i == N - 2) and (j == 0)) or ((i == 0) and (j == N - 2)) or ((i == N - 1) and (j == 1)) or ((i == 1) and (j == N - 1)):
 
+            # Robots have cross diaginal connections to reinforce the polygon formation.
             phi = (180 - 360 / N)
             theta = math.radians((180 - phi) / 2)
             C = 2 * math.cos(theta)
-            A[i,j] = C
+            A[i,j] = C 
 
         else:
 
-            A[i, j] = -1
+            #All others are treated as special conditions.
+            A[i, j] = -1 
 
 class Swarm_Simulation:
     def __init__(self):
@@ -65,85 +71,123 @@ class Swarm_Simulation:
 
         self.myfont = pygame.font.SysFont("arial", 25)
 
-        self.P = SCREENSIZE[1] * np.random.rand(Dim, N)  # Initial positions of robots in a 1000x1000 unit space
-        self.Pn = np.zeros((Dim, N), dtype=np.int)
+        self.P = SCREENSIZE[1] * np.random.rand(Dim, N)  # Initial positions of robots in the search space.
+        self.Pn = np.zeros((Dim, N), dtype=np.int) # The updated postions are initialized.
 
-        self.R = np.random.rand(N)  # Initial rotation of robots in 360 degrees
-        self.Rn = np.zeros((N), dtype=np.int)
+        self.R = np.random.rand(N)  # Initial rotation of robots in 2 pi radians.
+        self.Rn = np.zeros((N), dtype=np.int) # The updated rotations are initialized.
 
-        self.phi = np.zeros((N,N), dtype=np.float)
-        self.U = np.zeros((Dim, N), dtype=np.int)
+        self.phi = np.zeros((N,N), dtype=np.float) #Input angles to robot controllers.
+        self.U = np.zeros((Dim, N), dtype=np.int) #  Input desired movement direction for controllers
 
-        self.s = np.zeros((N), dtype=np.float)
+        self.s = np.zeros((N), dtype=np.float) #Desired speeds of each robot
 
-        self.min = 60
+        self.min = 40 #The target inter-robot distances
 
-        self.V_prev = 0
+        self.V_prev = 0 #The energy of the system.
 
-        self.delta_V = 0
+        self.delta_V = 0 # Change in energy of the system in one program loop.
 
-        self.step = 1.0
+        self.d = 1.0 #A counter to reduce spacings for non-connected robots
+
+        self.step = 0 #Counter increases whenever the energy begins to increase
+
+        self.m = 1 #Spacer multiplier
 
     def Run(self):
 
         while running:
-
-
-            self.V_prev = 0.5*V.sum()
+             
             self.screen.fill(black)
 
-            self.Pn = self.P
+            #The intial energy of the system.
+            self.V_prev = 0.5*V.sum()
+
+            #The positions and orientations of all bots are updated.
+            self.Pn = self.P 
             self.Rn = self.R
 
+            #Simulation cycles through all N bots
             for i in range(N):
                 for j in range(N):
 
-                    if (A[i,j] == 0):
+                    #Adjacency Matrix sets the Energy and Weight Matrix
+                    #Diagonal elements are zero.
+                    if (A[i,j] == 0): 
                         W[i, j] = 0
                         V[i, j] = 0
-                        
-                    elif A[i,j]== -1:
-                    
-                        if np.linalg.norm(self.Pn[:, i] - self.Pn[:, j]) < (N/(self.step))*self.min:
 
-                            if (self.delta_V <= 0):
+                    #Connections for Bots that are not connected intitally are dynamically set.
+                    elif A[i,j]== -1:
+
+                        #Non connected bots that get close together considered here.
+                        if np.linalg.norm(self.Pn[:, i] - self.Pn[:, j]) < ((N*self.m)/(self.d))*self.min:
+
+                            #If the energy is falling, non-connected bots are connected.
+                            if (self.delta_V < 0):
                                 
                                 pygame.draw.aaline(self.screen, red, self.P[:, i], self.P[:, j], 1)
-                                W[i, j] = K * (1 -  (N/(self.step))*self.min / np.linalg.norm(self.Pn[:, i] - self.Pn[:, j]))
-                                V[i, j] = 0.5 * K * (np.linalg.norm(self.Pn[:, i] - self.Pn[:, j]) - (N/3.0)* self.min) ** 2
+                                W[i, j] = K * (1 -  ((N*self.m)/(self.d))*self.min / np.linalg.norm(self.Pn[:, i] - self.Pn[:, j]))
+                                V[i, j] = 0.5 * K * (np.linalg.norm(self.Pn[:, i] - self.Pn[:, j]) - ((N*self.m)/(self.d))* self.min) ** 2
+
+                            #if the energy increases the spacing between bots decreases and a counter is increased.
                             else:
                             
-                                self.step += 0.01
+                                self.d += 0.01
+                                self.step += 1
+
+                                #If the counter hits a threshold, the counter and spacing divider is reset and the spacing multiplier increaes.
+                                if self.step > 1500:
+                                    self.d = 1.0
+                                    self.step = 0
+                                    self.m += 1
+
+                                    #Space multiplier is capped
+                                    if self.m > 5:
+                                        self.m = 1
+                                
                                 W[i, j] = 0
                                 V[i, j] = 0
+                        # Non-connected bots that are not close together stay unconnected
                         else:
                             W[i, j] = 0
                             V[i, j] = 0
 
+                    #Connected bots are assigned weights and energies.
                     else:
                         pygame.draw.aaline(self.screen, blue, self.P[:, i], self.P[:, j], 1)
                         W[i, j] = K * (1 - A[i,j] * self.min / np.linalg.norm(self.Pn[:, i] - self.Pn[:, j]))
                         V[i, j] = 0.5 * K *(np.linalg.norm(self.Pn[:, i] - self.Pn[:, j]) - A[i,j]* self.min) ** 2
 
-                    self.phi[i,j] = math.atan2(self.P[1,j] - self.P[1,i], self.P[0,j] - self.P[0,i]) -self.Rn[i] # Robot i's angle input
+                    # Robot i's angle input
+                    self.phi[i,j] = math.atan2(self.P[1,j] - self.P[1,i], self.P[0,j] - self.P[0,i]) -self.Rn[i] 
                     self.phi[i,i] = 0
 
+                    # Rotation is calculated
                     self.U[0, i] = np.linalg.norm(self.Pn[:, i] - self.Pn[:, j]) * math.cos(self.phi[i,j])*W[i,j]
                     self.U[1, i] = np.linalg.norm(self.Pn[:, i] - self.Pn[:, j]) * math.sin(self.phi[i,j])*W[i,j]
 
                     self.Rn[i] = self.Rn[i] + math.atan2(self.U[1, i], self.U[0, i])
 
-                    self.s[i] = 0.0005*V[i].sum()
+                    
+                    # Speeds are calculated based on the energy of neighbors
+                    self.s[i] = 0.005*V[i].sum()
 
+                    #Speeds are capped.
                     if self.s[i] > 100:
                         self.s[i] = 100
-
+ 
+                    # Bot positions are updated.
                     self.Pn[0, i] = self.Pn[0, i] + self.s[i] * math.cos(self.Rn[i]) * 0.005
-                    self.Pn[1, i] = self.Pn[1, i] + self.s[i] * math.sin(self.Rn[i]) * 0.005 
+                    self.Pn[1, i] = self.Pn[1, i] + self.s[i] * math.sin(self.Rn[i]) * 0.005
 
+                    # Bots are drawn
                     pygame.draw.circle(self.screen, green, [int(self.Pn[0, i]), int(self.Pn[1, i])], bot_radius, 1)
-                    
+
+                    #Change in energy is calculated
                     self.delta_V = int(0.5*V.sum() - self.V_prev)
+
+                    #print 0.5*V.sum()
 
 
             self.R = self.Rn
